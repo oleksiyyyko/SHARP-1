d84e11a039db3f29eaeda17c3979509855a1ae8d
oleksiyyyko <hrabovskiyalexey@gmail.com>
2024-12-11 22:05:49 +0300

    .

---
+++ PR-3/webAPI/webAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webAPI.Models;
+
+namespace webAPI.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+    }
+}
